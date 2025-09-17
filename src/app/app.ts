d0decd7b2101afa5607320c3d1230f16b52a234e
Mohamed Elekhnawy <em55599555@gmail.com>
2025-09-18 01:35:59 +0300

    save changes on product

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,7 @@ import { Navbar } from './Components/navbar/navbar';
 import { Footer } from './Components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HomepageComponent,Navbar,Footer],
+  imports: [RouterOutlet,Navbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
