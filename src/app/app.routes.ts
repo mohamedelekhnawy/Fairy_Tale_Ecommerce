8ea097cd34e2dcca19dab16ea062e55eedff7455
Mohamed Elekhnawy <em55599555@gmail.com>
2025-09-17 15:58:52 +0300

    update on nave bar and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in';
 import { Register } from './auth/register/register';
 import { Products } from './Components/products/products';
+import { HomepageComponent } from './Components/homepage/homepage.component';
 
 export const routes: Routes = [
-  
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component:HomepageComponent },
   { path: 'Login', component: SignInComponent },
   { path: 'Register', component: Register },
   { path: 'Products', component: Products },
