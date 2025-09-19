fde586c9c21a6a7cbb70c0577cc62d63c9430e42
Mohamed Elekhnawy <em55599555@gmail.com>
2025-09-19 03:46:54 +0300

    feat: add product details component with styling and functionality
    - Created product-details component including HTML, CSS, and TypeScript files.
    - Implemented image selection and related products display.
    - Added button directives for styling buttons in the component.
    - Developed product service to manage product data and retrieval.
    - Defined product model interface for type safety.

--- src/app/Directives/elment-scale.ts
+++ src/app/Directives/elment-scale.ts
@@ -9,7 +9,7 @@ export class ElmentScale {
     this.el.nativeElement.style.transition = 'all 0.2s ease-in-out';
   }
   @HostListener('mouseenter') onmousEente(){
-    this.el.nativeElement.style.transform = 'scale(1.05)';
+    this.el.nativeElement.style.transform = 'scale(1.03)';
     this.el.nativeElement.style.boxShadow = '0 10px 20px rgba(0,0,0,0.15)';
   }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignInComponent } from './auth/sign-in/sign-in';
 import { Register } from './auth/register/register';
 import { Products } from './Components/products/products';
 import { HomepageComponent } from './Components/homepage/homepage.component';
+import { ProductDetails } from './Components/product-details/product-details';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'Login', component: SignInComponent },
   { path: 'Register', component: Register },
   { path: 'Products', component: Products },
+  { path: 'product/:id', component: ProductDetails }
 ];
